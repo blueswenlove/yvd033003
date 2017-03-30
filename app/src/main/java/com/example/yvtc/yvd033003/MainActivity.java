e8175e8da3c922555d242a84acb9cb51712f4bc9
blueswen <blueswenlove@gamil.com>
2017-03-30 16:21:25 +0800

    check show item

--- app/src/main/java/com/example/yvtc/yvd033003/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvd033003/MainActivity.java
@@ -2,47 +2,66 @@ package com.example.yvtc.yvd033003;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    String data[] = {"蘋果","橘子","香蕉","檸檬"};
-    String data2[] = {"apple","lemon","banana","orange"};
+    ArrayList<String> data = new ArrayList<>();
+    MyAdapter adapter;
     ListView lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lv = (ListView)findViewById(R.id.listView);
-        MyAdapter adapter = new MyAdapter();
+        lv = (ListView) findViewById(R.id.listView);
+        data.add("AA1");
+        data.add("AA2");
+        data.add("AA3");
+        data.add("AA4");
+        data.add("AA5");
+        data.add("AA6");
+        data.add("AA7");
+        data.add("AA8");
+        data.add("AA9");
+        data.add("AA10");
+        data.add("AA11");
+        data.add("AA12");
+        adapter = new MyAdapter(MainActivity.this, data);
+
         lv.setAdapter(adapter);
     }
 
-    class MyAdapter extends BaseAdapter
-    {
-        @Override
-        public int getCount()  {
-            return data.length;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("Show");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getTitle().equals("Show"))
+        {
+            StringBuilder sb = new StringBuilder();
+            int i;
+            for (i=0;i<adapter.chks.length;i++)
+            {
+                if (adapter.chks[i])
+                {
+                    sb.append(data.get(i) + ",");
+                }
+            }
+            Toast.makeText(MainActivity.this, sb.toString(), Toast.LENGTH_LONG).show();
         }
-
-        @Override
-        public Object getItem(int position) { return null; }
-
-        @Override
-        public long getItemId(int position) { return 0;}
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View v = getLayoutInflater().inflate(R.layout.myitem,null);
-
-            TextView tv = (TextView) v.findViewById(R.id.textView);
-            TextView tv2 = (TextView) v.findViewById(R.id.textView2);
-            tv.setText(data[position]);
-            tv2.setText(data2[position]);
-            return v;
-        }
+        return super.onOptionsItemSelected(item);
     }
 }
+
+
